cbc44876fa1a5c1cfeb65d58396490d7efb27bdf
Douglas <douglasestevam234@gmail.com>
2022-08-06 10:48:12 -0300

    first commit

---
+++ src/app/npx-chart/npx-chart.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { BarsChartComponent } from './bars-chart/bars-chart.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [
+    PieChartComponent,
+    BarsChartComponent
+  ],
+  imports: [    
+    BrowserModule,
+    CommonModule,
+    NgApexchartsModule,
+    NpxChartModule
+  ]
+})
+export class NpxChartModule { }
